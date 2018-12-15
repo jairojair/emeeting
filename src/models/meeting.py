e9314bb94f9593e6d5e03943eb34d968684ad94b
JJ <jairojair@gmail.com>
2018-12-15 19:37:38 -0200

    Refact: add filter by date and room id in meetings model.

--- src/models/meeting.py
+++ src/models/meeting.py
@@ -1,7 +1,42 @@
+import re
 from orator import Model
+from orator.orm import scope
 
 
 class Meeting(Model):
 
     __visible__ = ["id", "title", "date_start", "date_end", "owner", "room_id"]
     __fillable__ = ["title", "date_start", "date_end", "owner", "room_id"]
+
+    @scope
+    def by_room_id(self, query, id):
+        """
+        Filter meetings by room id.
+        """
+
+        try:
+
+            if id is None:
+                return
+
+            id = int(id)
+            return query.where("room_id", id)
+
+        except ValueError:
+            raise Exception("The room_id need be a integer. Exemple: room_id: 100")
+
+    @scope
+    def by_date(self, query, date):
+        """
+        Describe
+        """
+
+        if date is None:
+            return
+
+        date_filter_format = r"(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})$"
+
+        if not re.match(date_filter_format, date):
+            raise Exception("The date filter format must be yyyy-mm-dd")
+
+        return query.where("date_start", "like", f"%{date}%")
