2166205ca293761d07e83830d97be850fc4d9527
JJ <jairojair@gmail.com>
2018-12-16 16:19:45 -0200

    Refact: datetime format to zalando guideline.

--- src/validators.py
+++ src/validators.py
@@ -6,19 +6,18 @@ from molten.validation.field import _T
 date_format = (
     r"(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})"
     r"[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})"
-    r"(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?"
-    r"(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$"
+    r"(?::(?P<second>\d{1,2}))?$"
 )
 
 
 class DateValidator:
     """
-    Date validator for pattern 2008-09-15T13:30:00+03:00
+    Date validator for pattern 2008-09-15T13:30:00
     """
 
     def validate(self, field: Field[_T], value: str):
 
         if not re.match(date_format, value):
-            raise FieldValidationError(f"must match pattern 2008-09-15T13:30:00+03:00")
+            raise FieldValidationError(f"must match pattern 2008-09-15T13:30:00")
 
         return value
--- tests/api/test_api_meetings.py
+++ tests/api/test_api_meetings.py
@@ -137,8 +137,8 @@ def test_create_meeting_with_wrong_date_format(client, number):
     assert response.status_code == 400
     assert response.json() == {
         "errors": {
-            "date_end": "must match pattern 2008-09-15T13:30:00+03:00",
-            "date_start": "must match pattern 2008-09-15T13:30:00+03:00",
+            "date_end": "must match pattern 2008-09-15T13:30:00",
+            "date_start": "must match pattern 2008-09-15T13:30:00",
         }
     }
 
@@ -149,8 +149,8 @@ def test_create_meeting_with_date_start_bigger_than_end(client, room):
 
     meeting_data = {
         "title": fake.text(10),
-        "date_start": "2009-09-15T13:30:00+03:00",
-        "date_end": "2008-09-15T13:30:00+03:00",
+        "date_start": "2009-09-15T13:30:00",
+        "date_end": "2008-09-15T13:30:00",
         "owner": fake.name(),
         "room_id": room.get("id"),
     }
